8f8ef2efdb73a243dc182aa390134ec23c71d7d1
partythenwork <55715618+partythenwork@users.noreply.github.com>
2019-10-05 10:22:43 -0500

    Update ohlc-graph.py

--- ohlc-graph.py
+++ ohlc-graph.py
@@ -3,12 +3,21 @@ import plotly.graph_objects as go
 import pandas as pd
 from datetime import datetime
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+#for data download
+#https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&outputsize=full&apikey=PIS5KN1SIW74TFZE&datatype=csv 
 
-fig = go.Figure(data=[go.Candlestick(x=df['Date'],
-                open=df['AAPL.Open'],
-                high=df['AAPL.High'],
-                low=df['AAPL.Low'],
-                close=df['AAPL.Close'])])
+#df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+df = pd.read_csv('MSFT.txt')
+period = 100
+EMA_name = 'EMA'+str(period)
 
+df['EMA'] = df['close'].ewm(span=period,min_periods=0,adjust=False).mean()
+
+
+fig = go.Figure(data=[go.Candlestick(x=df['timestamp'], open=df['open'], high=df['high'], low=df['low'], close=df['close'], name='MSFT'),
+                    go.Scatter(x=df['timestamp'], y=df['EMA'],name=EMA_name)
+                     ])
+                
 fig.show()
+
+print(df)
